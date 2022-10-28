7715b595bc3227f58c4a672bd083acbd9127c46b
Dane Dalton <danezdalton1@gmail.com>
2022-10-28 14:33:31 -0500

    figured out postgres user roles and db creation

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_10_12_024819) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: :cascade do |t|
     t.string "project_name"
     t.text "project_description"
